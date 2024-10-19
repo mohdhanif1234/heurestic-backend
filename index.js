1e2dd88350c4b2f5aa6a5366e161fbb71d595fdb
Mohammad Hanif <hanif.topia@gmail.com>
2024-10-19 13:13:27 +0530

    covered get request with query params, middlewares and post request in depth

--- index.js
+++ index.js
@@ -3,7 +3,32 @@ import { users } from "./constants.js"
 
 const app = express()
 
+
 const PORT = 8080
+let userCount = 0
+const mockUser = []
+
+const logMiddleware = (req, res, next) => {
+    userCount++
+    console.log('Mai logMiddlware mai aaya')
+    console.log('User count is---', userCount)
+    // return res.json({ msg: 'Bhaisahab mai pehle middleware se hi return ho gaya' })
+    next()
+}
+
+const logMiddleware2 = (req, res, next) => {
+    console.log('Mai dusre logMiddlware mai aaya')
+    const obj = {
+        name: 'hanif',
+        salary: 25
+    }
+
+    req.hanif = obj
+    // return res.json({ msg: 'Bhaisahab mai dusre middleware se hi return ho gaya' })
+    next()
+}
+// app.use(logMiddleware, logMiddleware2)
+app.use(express.json())
 
 // route for getting all users
 app.get('/', (req, res) => {
@@ -29,6 +54,48 @@ app.get('/:id', (req, res) => {
     const findUser = users.find(user => user.id === parsedId)
     if (!findUser) return res.json({ msg: 'User not found' })
     return res.json({ findUser })
+})
+
+// route for getting data by query params
+app.get('/api/filter', (req, res) => {
+    try {
+        const { username, email } = req.query
+
+        console.log('Username---', username)
+        console.log('Email---', email)
+
+        console.log('Request obj---', req.hanif)
+
+        //  throw new Error('Kuch error aa gaya bhai')
+
+        //  const findUser = users.find(user => user.username === username && user.email === email)
+
+        const findUser = users.filter(user => user.username.includes(username) || user.email.includes(email))
+
+        if (!findUser) return res.status(414).json({ msg: 'The user is not found' })
+
+        return res.status(200).json({
+            msg: 'The user was found',
+            findUser
+        })
+
+    } catch (error) {
+        return res.status(500).json({ msg: error.message })
+    }
+})
+
+app.post('/add/user', (req, res) => {
+    const { username, password } = req.body
+
+    console.log('Username----', username)
+    console.log('Password---', password)
+
+    mockUser.push({ username, password })
+
+    return res.status(201).json({
+        msg: 'User created successfully',
+        user: mockUser
+    });
 })
 
 app.listen(PORT, () => {
